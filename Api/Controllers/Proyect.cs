6b0c4f72184f5dc9ffd3e52115224de702c151e8
Julian <47957027+julian1010011010@users.noreply.github.com>
2022-11-01 22:25:56 -0500

    Initial commit

---
+++ Api/Controllers/Proyect.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+ 
+
+public class ProyectController : ControllerBase
+{
+
+    [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public IActionResult GetById(int id)
+    {
+        try
+        {
+            Dictionary<string, object> data = new Dictionary<string, object>
+            {
+                { "Javeriana", new {id= id*2 } }
+            };
+            return Ok(data);
+        }
+        catch (Exception e)
+        {
+
+            return BadRequest(e);
+        } 
+    }
+
+
+}
